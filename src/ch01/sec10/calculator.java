252e1141d7fea5a218cdc74de563deb5bb92397e
WATCHHONG <gihong0101@gmail.com>
2025-09-10 12:47:47 +0900

    first commit

---
+++ src/ch01/sec10/calculator.java
@@ -0,0 +1,12 @@
+package ch01.sec10;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 1;
+		int result = x + y;
+		System.out.println(result);
+	}
+
+}
