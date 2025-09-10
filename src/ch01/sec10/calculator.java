e59deb4ab444e86c54b3d1236da20cc3274097a5
WATCHHONG <gihong0101@gmail.com>
2025-09-10 13:59:19 +0900

    2nd week

--- src/ch01/sec10/calculator.java
+++ src/ch01/sec10/calculator.java
@@ -1,12 +1,12 @@
 package ch01.sec10;
 
-public class calculator {
+public class Calculator {
 
 	public static void main(String[] args) {
 		int x = 1;
 		int y = 1;
 		int result = x + y;
-		System.out.println(result);
+		System.out.println("/* 주석이 될 수 없음 */ \n" + result);
 	}
 
 }
